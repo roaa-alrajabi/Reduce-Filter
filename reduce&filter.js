ee1a6357ef875e3eae866a20b7fde87bebadd2e0
roaa-alrajabi <roaa.roaaalrajabi@gmail.com>
2019-07-27 21:40:53 +0300

    solve the filter  exercises

--- reduce&filter.js
+++ reduce&filter.js
@@ -1,4 +1,4 @@
-console.log('Here is : ', 'Reduce & Filter') \
+console.log('Here is : ', 'Reduce & Filter')
 
 //Using Reduce 1 -5
 var persons = [
@@ -77,17 +77,31 @@ and return an array of even number only
 
 Ex: evenOnly([1,8,6,4]) => [8,6,4]
 */
+function evenOnly(arr) {
+  // debugger
+  var result = arr.filter(function (element, index, array) {
+    return element %2===0
+  });
+  return result
+}
+
 
 
 /*
 7
 Create a function called multiFour
 that accept an array
-and return an array of these number that is a mutiply by 4
+and return an array of these number
+ that is a mutiply by 4
 
 Ex: multiFour([1,8,6,4]) => [8,4]
 */
-
+function multiFour(arr){
+ var result = arr.filter(function(element,index,array){
+ return element % 4 ===0;
+ });
+return result
+}
 
 /*
 8
@@ -100,6 +114,14 @@ and return an array of these string that contain this char
 Ex: containChar(["hello","world"],w) => ["world"]
 Ex: containChar(["hello","world"],l) => ["hello","world"]
 */
+function containChar(arr,char){
+
+  var result =arr.filter(function(element,index,array){
+  
+    return(element.includes(char)) 
+  });
+  return result
+}
 
 
 /*
@@ -111,8 +133,26 @@ and return an array that have the string with odd length in even index
 var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
 Ex: evenIndexOddLength(strings) => ["madrasa"]
 */
+var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
+
+function evenIndexOddLength(arr){
+  // debugger
+  var result =arr.filter(function(element,index,array){
+
+return ((element.length)%2!==0) && (index%2===0)
+
+  });
+return result
+}
 
 
+var persons = [
+  { name: { first: 'John', last: 'Hob' }, age: 35 },
+  { name: { first: 'Alex', last: 'Mercer' }, age: 25 },
+  { name: { first: 'Alice', last: 'Zaheer' }, age: 24 },
+  { name: { first: 'Zues', last: 'Odin' }, age: 55 },
+  { name: { first: 'Soso', last: 'Al-Amora' }, age: 67 }
+];
 /*
 10
 Using the varabile persons
@@ -122,7 +162,12 @@ and return the person that have age older than this number
 
 Ex: olderThan(persons,56) => [{ name: { first: 'Soso', last: 'Al-Amora' }, age: 67 }]
 */
-
+function olderThan(arr,num){
+  var result = arr.filter(function(element,index,array){
+ return (array[index].age)>=num 
+  });
+  return result
+}
 /*
 11
 Create a function called shorterThan
@@ -133,5 +178,14 @@ var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
 Ex: shorterThan(strings,5) => ["alex","emad","hala"]
 */
 
+var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
+
+function shorterThan(arr,num){
+
+  var result = arr.filter(function(element, index,array){
+  return (element.length<num)
+  });
+  return result
+}
 
 // if you finish the exercises review the material of the week and help your classmate
